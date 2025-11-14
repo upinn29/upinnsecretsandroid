1b989331570071f15119290c24e39dca939b8602
upinn29 <upinn.29@gmail.com>
2025-11-14 14:56:42 -0500

    FIX

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "upinn.tech"
                 artifactId = "upinnsecretsandroid"
-                version = "1.1.6"
+                version = "1.1.7"
 
                 from(components["release"])
 
